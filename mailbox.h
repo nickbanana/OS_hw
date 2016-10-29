0dcea2a53cb4f1b9fd5bb2612d49626787502c57
nickbanana <nick.chi@gmail.com>
2016-10-29 21:16:34 +0800

    add header

--- mailbox.h
+++ mailbox.h
@@ -1,3 +1,9 @@
+#ifndef __MAILBOX_H__
+#define __MAILBOX_H__
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 mailbox_t mailbox_open(int id);
 int mailbox_unlink(int id);
 int mailbox_close(mailbox_t box);
@@ -5,3 +11,4 @@ int mailbox_send(mailbox_t box, mail_t *mail);
 int mailbox_recv(mailbox_t box, mail_t *mail);
 int mailbox_check_empty(mailbox_t box);
 int mailbox_check_full(mailbox_t box);
+#endif
